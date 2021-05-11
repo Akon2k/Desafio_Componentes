d849d6d276e04ee3d6f0353e14d28173f5af8c99
Iván Carmona Vidal <ivancarmonavidal@gmail.com>
2021-05-11 13:11:49 -0400

    componenes fin

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div v-for="(personaje, i) in personajes" :key="i" class="personaje">
+      <img :src="personaje.image" />
+      <div style="padding-top: 60px">
+        <h2>{{ personaje.name }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      personajes: [
+        {
+          name: "Rick Sanchez",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        },
+        {
+          name: "Morty Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        },
+        {
+          name: "Summer Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        },
+        {
+          name: "Beth Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        },
+        {
+          name: "Jerry Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        },
+        {
+          name: "Abadango Cluster Princess",
+          image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +53,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.personaje {
+  display: flex;
+  margin: 15px;
+}
+
+.personaje img {
+  width: 125px;
+  border-radius: 50%;
+}
+
+.personaje h2 {
+  color: #ff0000;
 }
 </style>
